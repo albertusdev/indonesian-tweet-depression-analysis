aea83abdc0a8d4adf732961ec1ec404ee968506c
albertusangga <albertusangga.98@gmail.com>
2019-05-27 21:31:12 +0700

    Add options handler

--- back_end/views.py
+++ back_end/views.py
@@ -1,13 +1,19 @@
 import json
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods
 
 from back_end.apps import App
 
 
-@require_http_methods(['POST'])
+@require_http_methods(['POST', 'OPTIONS'])
 def get_depression_prediction(request):
+    if request.method == "OPTIONS" or request.method == "":
+        allowed_methods = ['post', 'options']
+        response = HttpResponse()
+        response['allow'] = ','.join(allowed_methods)
+        return response
+
     print(request.body)
     json_data = json.loads(request.body)
     tweet = json_data['tweet']
